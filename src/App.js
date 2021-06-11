7cfdd3ae933d2b5aa2521029bbd421f13c355ccf
sidney-souza <83144292+sidney-souza@users.noreply.github.com>
2021-06-11 11:14:49 -0300

    carrinho

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CarrinhoCompras from './components/CarrinhoDeCompras/CarrinhoCompras.jsx';
 import Produto from './components/Produto/Produto.jsx'
 
 export class App extends React.Component {
@@ -11,13 +12,39 @@ export class App extends React.Component {
           {titulo: "d", preco: 1, img: 4},
     ],
     carrinho: [
-
+      {
+        id: 4,
+        nome: 'Produto 4',
+        preco: 10,
+        img: 'https://picsum.photos/200/200?a=4',
+        quantidade: 1
+      },
+      {
+        id: 3,
+        nome: 'Produto 3',
+        preco: 30,
+        img: 'https://picsum.photos/200/200?a=3',
+        quantidade: 2
+      }
     ],
   }
 
 
   funcaoAdiciona = () => {
 
+  }
+
+  excluirProdutoDoCarrinho = (itemId) => {
+    const novoCarrinho = this.state.carrinho.map((produto) => {
+      if(produto.id === itemId) {
+        return{
+          ...produto,
+          quantidade: produto.quantidade -1
+        }
+      }
+      return produto
+    }).filter((produto) => produto.quantidade > 0)
+    this.setState({carrinho:novoCarrinho})
   }
   
   render () {
@@ -41,8 +68,12 @@ export class App extends React.Component {
           adiciona={()=>{this.funcaoAdiciona()}}
           />
           
+         
           
-          {/*<Carrinho> </Carrinho>*/}
+          <CarrinhoCompras
+          itens= {this.state.carrinho}
+          excluirProdutoDoCarrinho={this.excluirProdutoDoCarrinho}
+          />
 
         </main>
 
